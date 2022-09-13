91c18a88218fdc900219015dad7a9eab6f6250ec
Larii04 <larissagfandrade@gmail.com>
2022-09-13 13:49:48 -0300

    Códigos
    Contém todos os arquivos do programa.

---
+++ Exercícios/TP02/TP02/TP02/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace TP02
+{
+    partial class Frm_TP02
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Txt_Deslocamento = new System.Windows.Forms.TextBox();
+            this.Txt_Angulo = new System.Windows.Forms.TextBox();
+            this.Txt_Forca = new System.Windows.Forms.TextBox();
+            this.Txt_coef = new System.Windows.Forms.TextBox();
+            this.Txt_Massa = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Btn_Sair = new System.Windows.Forms.Button();
+            this.Btn_Limpar = new System.Windows.Forms.Button();
+            this.Btn_Calcular = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Txt_FAtrito = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Txt_Deslocamento);
+            this.groupBox1.Controls.Add(this.Txt_Angulo);
+            this.groupBox1.Controls.Add(this.Txt_Forca);
+            this.groupBox1.Controls.Add(this.Txt_coef);
+            this.groupBox1.Controls.Add(this.Txt_Massa);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 257);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informações do Corpo";
+            // 
+            // Txt_Deslocamento
+            // 
+            this.Txt_Deslocamento.Location = new System.Drawing.Point(184, 206);
+            this.Txt_Deslocamento.MaxLength = 5;
+            this.Txt_Deslocamento.Name = "Txt_Deslocamento";
+            this.Txt_Deslocamento.Size = new System.Drawing.Size(64, 30);
+            this.Txt_Deslocamento.TabIndex = 13;
+            // 
+            // Txt_Angulo
+            // 
+            this.Txt_Angulo.Location = new System.Drawing.Point(184, 164);
+            this.Txt_Angulo.MaxLength = 5;
+            this.Txt_Angulo.Name = "Txt_Angulo";
+            this.Txt_Angulo.Size = new System.Drawing.Size(64, 30);
+            this.Txt_Angulo.TabIndex = 12;
+            // 
+            // Txt_Forca
+            // 
+            this.Txt_Forca.Location = new System.Drawing.Point(184, 120);
+            this.Txt_Forca.MaxLength = 5;
+            this.Txt_Forca.Name = "Txt_Forca";
+            this.Txt_Forca.Size = new System.Drawing.Size(64, 30);
+            this.Txt_Forca.TabIndex = 11;
+            // 
+            // Txt_coef
+            // 
+            this.Txt_coef.Location = new System.Drawing.Point(184, 79);
+            this.Txt_coef.MaxLength = 5;
+            this.Txt_coef.Name = "Txt_coef";
+            this.Txt_coef.Size = new System.Drawing.Size(64, 30);
+            this.Txt_coef.TabIndex = 10;
+            // 
+            // Txt_Massa
+            // 
+            this.Txt_Massa.Location = new System.Drawing.Point(184, 40);
+            this.Txt_Massa.MaxLength = 5;
+            this.Txt_Massa.Name = "Txt_Massa";
+            this.Txt_Massa.Size = new System.Drawing.Size(64, 30);
+            this.Txt_Massa.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(11, 120);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 25);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Força";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(254, 206);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "(m)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Deslocamento";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(254, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "( ° )";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ângulo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "(N)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Coef. de Atrito";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "(kg)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Massa do Corpo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.Btn_Sair);
+            this.panel1.Controls.Add(this.Btn_Limpar);
+            this.panel1.Controls.Add(this.Btn_Calcular);
+            this.panel1.Location = new System.Drawing.Point(12, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(317, 71);
+            this.panel1.TabIndex = 1;
+            // 
+            // Btn_Sair
+            // 
+            this.Btn_Sair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Sair.Location = new System.Drawing.Point(207, 13);
+            this.Btn_Sair.Name = "Btn_Sair";
+            this.Btn_Sair.Size = new System.Drawing.Size(89, 39);
+            this.Btn_Sair.TabIndex = 2;
+            this.Btn_Sair.Text = "&Sair";
+            this.Btn_Sair.UseVisualStyleBackColor = true;
+            this.Btn_Sair.Click += new System.EventHandler(this.Btn_Sair_Click);
+            // 
+            // Btn_Limpar
+            // 
+            this.Btn_Limpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Limpar.Location = new System.Drawing.Point(109, 13);
+            this.Btn_Limpar.Name = "Btn_Limpar";
+            this.Btn_Limpar.Size = new System.Drawing.Size(89, 39);
+            this.Btn_Limpar.TabIndex = 1;
+            this.Btn_Limpar.Text = "&Limpar";
+            this.Btn_Limpar.UseVisualStyleBackColor = true;
+            this.Btn_Limpar.Click += new System.EventHandler(this.Btn_Limpar_Click);
+            // 
+            // Btn_Calcular
+            // 
+            this.Btn_Calcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Calcular.Location = new System.Drawing.Point(14, 13);
+            this.Btn_Calcular.Name = "Btn_Calcular";
+            this.Btn_Calcular.Size = new System.Drawing.Size(89, 39);
+            this.Btn_Calcular.TabIndex = 0;
+            this.Btn_Calcular.Text = "&Calcular";
+            this.Btn_Calcular.UseVisualStyleBackColor = true;
+            this.Btn_Calcular.Click += new System.EventHandler(this.Btn_Calcular_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.Txt_FAtrito);
+            this.groupBox2.Location = new System.Drawing.Point(13, 347);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(316, 77);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Força de Atrito";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(231, 34);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 25);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "(N)";
+            // 
+            // Txt_FAtrito
+            // 
+            this.Txt_FAtrito.Location = new System.Drawing.Point(63, 34);
+            this.Txt_FAtrito.Name = "Txt_FAtrito";
+            this.Txt_FAtrito.Size = new System.Drawing.Size(162, 30);
+            this.Txt_FAtrito.TabIndex = 0;
+            // 
+            // Frm_TP02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(339, 423);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm_TP02";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Força de Atrito de um Corpo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox Txt_Deslocamento;
+        private System.Windows.Forms.TextBox Txt_Angulo;
+        private System.Windows.Forms.TextBox Txt_Forca;
+        private System.Windows.Forms.TextBox Txt_coef;
+        private System.Windows.Forms.TextBox Txt_Massa;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn_Sair;
+        private System.Windows.Forms.Button Btn_Limpar;
+        private System.Windows.Forms.Button Btn_Calcular;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox Txt_FAtrito;
+    }
+}
+
---
+++ Exercícios/TP02/TP02/TP02/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP02
+{
+    public partial class Frm_TP02 : Form
+    {
+        #region Variáveis Globais
+        string msg, titulo;
+        MessageBoxButtons botoes;
+        MessageBoxIcon icone;
+        MessageBoxDefaultButton definido;
+        #endregion
+        public Frm_TP02()
+        {
+            InitializeComponent();
+        }
+        #region Botão Sair
+        private void Btn_Sair_Click(object sender, EventArgs e)
+        {
+            msg = "Tem certeza?";
+            titulo = "Sair do programa...";
+            botoes = MessageBoxButtons.YesNo;
+            icone = MessageBoxIcon.Question;
+            definido = MessageBoxDefaultButton.Button2;
+            if (MessageBox.Show(msg, titulo, botoes, icone, definido) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        #endregion
+        #region Botão Limpar
+        private void Btn_Limpar_Click(object sender, EventArgs e)
+        {
+            msg = "Confirma?";
+            titulo = "Limpar os dados...";
+            botoes = MessageBoxButtons.YesNo;
+            icone = MessageBoxIcon.Information;
+            definido = MessageBoxDefaultButton.Button2;
+            if (MessageBox.Show(msg, titulo, botoes, icone, definido) == DialogResult.Yes)
+            {
+                Txt_Massa.Clear();
+                Txt_coef.Clear();
+                Txt_Forca.Clear();
+                Txt_Angulo.Clear();
+                Txt_Deslocamento.Clear();
+                Txt_FAtrito.Clear();
+            }
+        }
+
+       
+        #endregion
+
+        #region Botão Calcular
+        private void Btn_Calcular_Click(object sender, EventArgs e)
+        {
+            double m, c, f, agr, ard, d, fa;
+            const double g = 9.8;
+
+            m = double.Parse(Txt_Massa.Text);
+            c = double.Parse(Txt_coef.Text);
+            f = double.Parse(Txt_Forca.Text);
+            agr = double.Parse(Txt_Angulo.Text);
+            ard = agr * Math.PI / 180;
+            d = double.Parse(Txt_Deslocamento.Text);
+            fa = c * (m * g - f * Math.Sin(ard)) * d * Math.Cos(ard);
+            Txt_FAtrito.Text = Convert.ToString(fa);
+        }
+        #endregion
+        #region LIXO
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
