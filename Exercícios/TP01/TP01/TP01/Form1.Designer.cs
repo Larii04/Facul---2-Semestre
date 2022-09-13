fb9486fc45fd9cd533c29ec4b14f0aef1fd2dd94
Larii04 <larissagfandrade@gmail.com>
2022-09-13 12:23:22 -0300

    Códigos
    Contém todos os arquivos do programa.

---
+++ Exercícios/TP01/TP01/TP01/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace TP01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Txt_R1 = new System.Windows.Forms.TextBox();
+            this.Txt_R2 = new System.Windows.Forms.TextBox();
+            this.Txt_R3 = new System.Windows.Forms.TextBox();
+            this.Txt_R4 = new System.Windows.Forms.TextBox();
+            this.Txt_R5 = new System.Windows.Forms.TextBox();
+            this.Txt_R6 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Btn_Calcular = new System.Windows.Forms.Button();
+            this.Btn_Limpar = new System.Windows.Forms.Button();
+            this.Bttn_Sair = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Txt_R3);
+            this.groupBox1.Controls.Add(this.Txt_R2);
+            this.groupBox1.Controls.Add(this.Txt_R1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(190, 209);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Circuito Delta";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Txt_R6);
+            this.groupBox2.Controls.Add(this.Txt_R5);
+            this.groupBox2.Controls.Add(this.Txt_R4);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(227, 15);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(195, 209);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Circuito Y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "R1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "R2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "R3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "R4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 25);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "R5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 25);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "R6";
+            // 
+            // Txt_R1
+            // 
+            this.Txt_R1.Location = new System.Drawing.Point(71, 44);
+            this.Txt_R1.Name = "Txt_R1";
+            this.Txt_R1.Size = new System.Drawing.Size(86, 30);
+            this.Txt_R1.TabIndex = 3;
+            // 
+            // Txt_R2
+            // 
+            this.Txt_R2.Location = new System.Drawing.Point(71, 92);
+            this.Txt_R2.Name = "Txt_R2";
+            this.Txt_R2.Size = new System.Drawing.Size(86, 30);
+            this.Txt_R2.TabIndex = 4;
+            // 
+            // Txt_R3
+            // 
+            this.Txt_R3.Location = new System.Drawing.Point(71, 141);
+            this.Txt_R3.Name = "Txt_R3";
+            this.Txt_R3.Size = new System.Drawing.Size(86, 30);
+            this.Txt_R3.TabIndex = 5;
+            // 
+            // Txt_R4
+            // 
+            this.Txt_R4.Location = new System.Drawing.Point(82, 42);
+            this.Txt_R4.Name = "Txt_R4";
+            this.Txt_R4.Size = new System.Drawing.Size(86, 30);
+            this.Txt_R4.TabIndex = 4;
+            // 
+            // Txt_R5
+            // 
+            this.Txt_R5.Location = new System.Drawing.Point(82, 92);
+            this.Txt_R5.Name = "Txt_R5";
+            this.Txt_R5.Size = new System.Drawing.Size(86, 30);
+            this.Txt_R5.TabIndex = 5;
+            // 
+            // Txt_R6
+            // 
+            this.Txt_R6.Location = new System.Drawing.Point(82, 141);
+            this.Txt_R6.Name = "Txt_R6";
+            this.Txt_R6.Size = new System.Drawing.Size(86, 30);
+            this.Txt_R6.TabIndex = 6;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Bttn_Sair);
+            this.groupBox3.Controls.Add(this.Btn_Limpar);
+            this.groupBox3.Controls.Add(this.Btn_Calcular);
+            this.groupBox3.Location = new System.Drawing.Point(16, 235);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(405, 79);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Controle";
+            // 
+            // Btn_Calcular
+            // 
+            this.Btn_Calcular.Location = new System.Drawing.Point(6, 29);
+            this.Btn_Calcular.Name = "Btn_Calcular";
+            this.Btn_Calcular.Size = new System.Drawing.Size(108, 33);
+            this.Btn_Calcular.TabIndex = 0;
+            this.Btn_Calcular.Text = "&Calcular";
+            this.Btn_Calcular.UseVisualStyleBackColor = true;
+            this.Btn_Calcular.Click += new System.EventHandler(this.Btn_Calcular_Click);
+            // 
+            // Btn_Limpar
+            // 
+            this.Btn_Limpar.Location = new System.Drawing.Point(130, 29);
+            this.Btn_Limpar.Name = "Btn_Limpar";
+            this.Btn_Limpar.Size = new System.Drawing.Size(117, 33);
+            this.Btn_Limpar.TabIndex = 1;
+            this.Btn_Limpar.Text = "&Limpar";
+            this.Btn_Limpar.UseVisualStyleBackColor = true;
+            this.Btn_Limpar.Click += new System.EventHandler(this.Btn_Limpar_Click);
+            // 
+            // Bttn_Sair
+            // 
+            this.Bttn_Sair.Location = new System.Drawing.Point(270, 29);
+            this.Bttn_Sair.Name = "Bttn_Sair";
+            this.Bttn_Sair.Size = new System.Drawing.Size(109, 33);
+            this.Bttn_Sair.TabIndex = 2;
+            this.Bttn_Sair.Text = "&Sair";
+            this.Bttn_Sair.UseVisualStyleBackColor = true;
+            this.Bttn_Sair.Click += new System.EventHandler(this.Bttn_Sair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(444, 320);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversão de Circuitos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox Txt_R3;
+        private System.Windows.Forms.TextBox Txt_R2;
+        private System.Windows.Forms.TextBox Txt_R1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox Txt_R6;
+        private System.Windows.Forms.TextBox Txt_R5;
+        private System.Windows.Forms.TextBox Txt_R4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button Bttn_Sair;
+        private System.Windows.Forms.Button Btn_Limpar;
+        private System.Windows.Forms.Button Btn_Calcular;
+    }
+}
+
---
+++ Exercícios/TP01/TP01/TP01/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bttn_Sair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Btn_Limpar_Click(object sender, EventArgs e)
+        {
+            Txt_R1.Clear();
+            Txt_R2.Clear();
+            Txt_R3.Clear();
+            Txt_R4.Clear();
+            Txt_R5.Clear();
+            Txt_R6.Clear();
+        }
+
+        private void Btn_Calcular_Click(object sender, EventArgs e)
+        {
+            double r1, r2, r3, r4, r5, r6, aux;
+            r1 = double.Parse(Txt_R1.Text);
+            r2 = double.Parse(Txt_R2.Text);
+            r3 = double.Parse(Txt_R3.Text);
+            aux = r1 + r2 + r3;
+            r4 = (r1 * r2) / aux;
+            r5 = (r2 * r3) / aux;
+            r6 = (r3 * r1) / aux;
+            Txt_R4.Text = Convert.ToString(r4);
+            Txt_R5.Text = Convert.ToString(r5);
+            Txt_R6.Text = Convert.ToString(r6);
+        }
+    }
+}
