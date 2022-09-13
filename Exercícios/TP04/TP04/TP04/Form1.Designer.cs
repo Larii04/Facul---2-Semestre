e7e73f5fe52b91a416ae45515f67a700e0461456
Larii04 <larissagfandrade@gmail.com>
2022-09-13 14:01:59 -0300

    Códigos
    Contém todos os arquivos do programa.

---
+++ Exercícios/TP04/TP04/TP04/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace TP04
+{
+    partial class Frm_TP04
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Btn_33 = new System.Windows.Forms.Button();
+            this.Btn_32 = new System.Windows.Forms.Button();
+            this.Btn_31 = new System.Windows.Forms.Button();
+            this.Btn_23 = new System.Windows.Forms.Button();
+            this.Btn_22 = new System.Windows.Forms.Button();
+            this.Btn_21 = new System.Windows.Forms.Button();
+            this.Btn_13 = new System.Windows.Forms.Button();
+            this.Btn_12 = new System.Windows.Forms.Button();
+            this.Btn_11 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Lbl_QuemJoga = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Lbl_Jo = new System.Windows.Forms.Label();
+            this.Lbl_Jx = new System.Windows.Forms.Label();
+            this.Btn_NovoJogo = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.Btn_33);
+            this.panel1.Controls.Add(this.Btn_32);
+            this.panel1.Controls.Add(this.Btn_31);
+            this.panel1.Controls.Add(this.Btn_23);
+            this.panel1.Controls.Add(this.Btn_22);
+            this.panel1.Controls.Add(this.Btn_21);
+            this.panel1.Controls.Add(this.Btn_13);
+            this.panel1.Controls.Add(this.Btn_12);
+            this.panel1.Controls.Add(this.Btn_11);
+            this.panel1.Location = new System.Drawing.Point(12, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 255);
+            this.panel1.TabIndex = 9;
+            // 
+            // Btn_33
+            // 
+            this.Btn_33.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_33.ForeColor = System.Drawing.Color.Red;
+            this.Btn_33.Location = new System.Drawing.Point(165, 173);
+            this.Btn_33.Name = "Btn_33";
+            this.Btn_33.Size = new System.Drawing.Size(60, 60);
+            this.Btn_33.TabIndex = 17;
+            this.Btn_33.UseVisualStyleBackColor = true;
+            this.Btn_33.Click += new System.EventHandler(this.Btn_33_Click);
+            // 
+            // Btn_32
+            // 
+            this.Btn_32.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_32.ForeColor = System.Drawing.Color.Red;
+            this.Btn_32.Location = new System.Drawing.Point(89, 173);
+            this.Btn_32.Name = "Btn_32";
+            this.Btn_32.Size = new System.Drawing.Size(60, 60);
+            this.Btn_32.TabIndex = 16;
+            this.Btn_32.UseVisualStyleBackColor = true;
+            this.Btn_32.Click += new System.EventHandler(this.Btn_32_Click);
+            // 
+            // Btn_31
+            // 
+            this.Btn_31.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_31.ForeColor = System.Drawing.Color.Red;
+            this.Btn_31.Location = new System.Drawing.Point(12, 173);
+            this.Btn_31.Name = "Btn_31";
+            this.Btn_31.Size = new System.Drawing.Size(60, 60);
+            this.Btn_31.TabIndex = 15;
+            this.Btn_31.UseVisualStyleBackColor = true;
+            this.Btn_31.Click += new System.EventHandler(this.Btn_31_Click);
+            // 
+            // Btn_23
+            // 
+            this.Btn_23.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_23.ForeColor = System.Drawing.Color.Red;
+            this.Btn_23.Location = new System.Drawing.Point(165, 97);
+            this.Btn_23.Name = "Btn_23";
+            this.Btn_23.Size = new System.Drawing.Size(60, 60);
+            this.Btn_23.TabIndex = 14;
+            this.Btn_23.UseVisualStyleBackColor = true;
+            this.Btn_23.Click += new System.EventHandler(this.Btn_23_Click);
+            // 
+            // Btn_22
+            // 
+            this.Btn_22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_22.ForeColor = System.Drawing.Color.Red;
+            this.Btn_22.Location = new System.Drawing.Point(89, 97);
+            this.Btn_22.Name = "Btn_22";
+            this.Btn_22.Size = new System.Drawing.Size(60, 60);
+            this.Btn_22.TabIndex = 13;
+            this.Btn_22.UseVisualStyleBackColor = true;
+            this.Btn_22.Click += new System.EventHandler(this.Btn_22_Click);
+            // 
+            // Btn_21
+            // 
+            this.Btn_21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_21.ForeColor = System.Drawing.Color.Red;
+            this.Btn_21.Location = new System.Drawing.Point(12, 97);
+            this.Btn_21.Name = "Btn_21";
+            this.Btn_21.Size = new System.Drawing.Size(60, 60);
+            this.Btn_21.TabIndex = 12;
+            this.Btn_21.UseVisualStyleBackColor = true;
+            this.Btn_21.Click += new System.EventHandler(this.Btn_21_Click);
+            // 
+            // Btn_13
+            // 
+            this.Btn_13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_13.ForeColor = System.Drawing.Color.Red;
+            this.Btn_13.Location = new System.Drawing.Point(165, 19);
+            this.Btn_13.Name = "Btn_13";
+            this.Btn_13.Size = new System.Drawing.Size(60, 60);
+            this.Btn_13.TabIndex = 11;
+            this.Btn_13.UseVisualStyleBackColor = true;
+            this.Btn_13.Click += new System.EventHandler(this.Btn_13_Click);
+            // 
+            // Btn_12
+            // 
+            this.Btn_12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_12.ForeColor = System.Drawing.Color.Red;
+            this.Btn_12.Location = new System.Drawing.Point(89, 19);
+            this.Btn_12.Name = "Btn_12";
+            this.Btn_12.Size = new System.Drawing.Size(60, 60);
+            this.Btn_12.TabIndex = 10;
+            this.Btn_12.UseVisualStyleBackColor = true;
+            this.Btn_12.Click += new System.EventHandler(this.Btn_12_Click);
+            // 
+            // Btn_11
+            // 
+            this.Btn_11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_11.ForeColor = System.Drawing.Color.Red;
+            this.Btn_11.Location = new System.Drawing.Point(12, 19);
+            this.Btn_11.Name = "Btn_11";
+            this.Btn_11.Size = new System.Drawing.Size(60, 60);
+            this.Btn_11.TabIndex = 9;
+            this.Btn_11.UseVisualStyleBackColor = true;
+            this.Btn_11.Click += new System.EventHandler(this.Btn_11_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Lbl_QuemJoga);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 76);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Quem joga...";
+            // 
+            // Lbl_QuemJoga
+            // 
+            this.Lbl_QuemJoga.ForeColor = System.Drawing.Color.Red;
+            this.Lbl_QuemJoga.Location = new System.Drawing.Point(17, 36);
+            this.Lbl_QuemJoga.Name = "Lbl_QuemJoga";
+            this.Lbl_QuemJoga.Size = new System.Drawing.Size(210, 26);
+            this.Lbl_QuemJoga.TabIndex = 0;
+            this.Lbl_QuemJoga.Text = "Jogador X";
+            this.Lbl_QuemJoga.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Lbl_Jo);
+            this.groupBox2.Controls.Add(this.Lbl_Jx);
+            this.groupBox2.Location = new System.Drawing.Point(14, 355);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(241, 120);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Placar";
+            // 
+            // Lbl_Jo
+            // 
+            this.Lbl_Jo.AutoSize = true;
+            this.Lbl_Jo.Location = new System.Drawing.Point(29, 81);
+            this.Lbl_Jo.Name = "Lbl_Jo";
+            this.Lbl_Jo.Size = new System.Drawing.Size(174, 29);
+            this.Lbl_Jo.TabIndex = 1;
+            this.Lbl_Jo.Text = "Jogador O = 0";
+            // 
+            // Lbl_Jx
+            // 
+            this.Lbl_Jx.AutoSize = true;
+            this.Lbl_Jx.Location = new System.Drawing.Point(28, 41);
+            this.Lbl_Jx.Name = "Lbl_Jx";
+            this.Lbl_Jx.Size = new System.Drawing.Size(172, 29);
+            this.Lbl_Jx.TabIndex = 0;
+            this.Lbl_Jx.Text = "Jogador X = 0";
+            // 
+            // Btn_NovoJogo
+            // 
+            this.Btn_NovoJogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_NovoJogo.ForeColor = System.Drawing.Color.Red;
+            this.Btn_NovoJogo.Location = new System.Drawing.Point(20, 488);
+            this.Btn_NovoJogo.Name = "Btn_NovoJogo";
+            this.Btn_NovoJogo.Size = new System.Drawing.Size(234, 37);
+            this.Btn_NovoJogo.TabIndex = 12;
+            this.Btn_NovoJogo.Text = "Reiniciar Jogo";
+            this.Btn_NovoJogo.UseVisualStyleBackColor = true;
+            this.Btn_NovoJogo.Click += new System.EventHandler(this.Btn_NovoJogo_Click);
+            // 
+            // Frm_TP04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(269, 544);
+            this.Controls.Add(this.Btn_NovoJogo);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm_TP04";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            this.Load += new System.EventHandler(this.Frm_TP04_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Btn_33;
+        private System.Windows.Forms.Button Btn_32;
+        private System.Windows.Forms.Button Btn_31;
+        private System.Windows.Forms.Button Btn_23;
+        private System.Windows.Forms.Button Btn_22;
+        private System.Windows.Forms.Button Btn_21;
+        private System.Windows.Forms.Button Btn_13;
+        private System.Windows.Forms.Button Btn_12;
+        private System.Windows.Forms.Button Btn_11;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Lbl_QuemJoga;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label Lbl_Jo;
+        private System.Windows.Forms.Label Lbl_Jx;
+        private System.Windows.Forms.Button Btn_NovoJogo;
+    }
+}
+
---
+++ Exercícios/TP04/TP04/TP04/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP04
+{
+    public partial class Frm_TP04 : Form
+    {
+        
+        #region Variáveis Globias
+        byte jx, venceX = 0, venceO = 0;
+        string letra;
+        #endregion
+        public Frm_TP04()
+        {
+            InitializeComponent();
+        }
+
+        #region Rotina para Inicio do Jogo
+        void inicio()
+        {
+            Lbl_QuemJoga.Text = "Jogador X";
+            Btn_11.Text = ""; Btn_12.Text = ""; Btn_13.Text = "";
+            Btn_21.Text = ""; Btn_22.Text = ""; Btn_23.Text = "";
+            Btn_31.Text = ""; Btn_32.Text = ""; Btn_33.Text = "";
+            Btn_11.Enabled = true; Btn_12.Enabled = true; Btn_13.Enabled = true;
+            Btn_21.Enabled = true; Btn_22.Enabled = true; Btn_23.Enabled = true;
+            Btn_31.Enabled = true; Btn_32.Enabled = true; Btn_33.Enabled = true;
+
+        }
+        #endregion
+        #region Start do Form
+        private void Frm_TP04_Load(object sender, EventArgs e)
+        {
+            jx = 1;
+        }
+        #endregion
+        #region Botão Reiniciar Jogo
+        private void Btn_NovoJogo_Click(object sender, EventArgs e)
+        {
+            inicio();
+            Lbl_Jx.Text = "Jogador X = 0";
+            Lbl_Jo.Text = "Jogador O = 0";
+            jx = 1; venceO = 0; venceX = 0;
+        }
+        #endregion
+        #region Controle dos botões do Jogo da Velha
+        private void Btn_11_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_11.Text = "X"; jx = 0;
+                Btn_11.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_11.Text = "O"; jx = 1;
+                Btn_11.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+
+        private void Btn_12_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_12.Text = "X"; jx = 0;
+                Btn_12.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_12.Text = "O"; jx = 1;
+                Btn_12.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+
+        private void Btn_13_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_13.Text = "X"; jx = 0;
+                Btn_13.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_13.Text = "O"; jx = 1;
+                Btn_13.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+
+        private void Btn_21_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_21.Text = "X"; jx = 0;
+                Btn_21.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_21.Text = "O"; jx = 1;
+                Btn_21.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+
+        private void Btn_22_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_22.Text = "X"; jx = 0;
+                Btn_22.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_22.Text = "O"; jx = 1;
+                Btn_22.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+
+        private void Btn_23_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_23.Text = "X"; jx = 0;
+                Btn_23.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_23.Text = "O"; jx = 1;
+                Btn_23.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+
+        private void Btn_31_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_31.Text = "X"; jx = 0;
+                Btn_31.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_31.Text = "O"; jx = 1;
+                Btn_31.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+
+        private void Btn_32_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_32.Text = "X"; jx = 0;
+                Btn_32.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_32.Text = "O"; jx = 1;
+                Btn_32.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+
+        private void Btn_33_Click(object sender, EventArgs e)
+        {
+            if (jx == 1)
+            {
+                Btn_33.Text = "X"; jx = 0;
+                Btn_33.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador O";
+            }
+            else
+            {
+                Btn_33.Text = "O"; jx = 1;
+                Btn_33.Enabled = false;
+                Lbl_QuemJoga.Text = "Jogador X";
+            }
+            confere();
+        }
+        #endregion
+        #region Confere as jogadas possíveis
+        void confere()
+        {
+            if (Btn_11.Text == Btn_12.Text && Btn_12.Text == Btn_13.Text)
+            {
+                letra = Btn_11.Text; ponto();
+            }
+            if (Btn_21.Text == Btn_22.Text && Btn_22.Text == Btn_23.Text)
+            {
+                letra = Btn_21.Text; ponto();
+            }
+            if (Btn_31.Text == Btn_32.Text && Btn_32.Text == Btn_33.Text )
+            {
+                letra = Btn_31.Text; ponto();
+            }
+            if (Btn_11.Text == Btn_21.Text && Btn_21.Text == Btn_31.Text )
+            {
+                letra = Btn_11.Text; ponto();
+            }
+            if (Btn_12.Text == Btn_22.Text && Btn_22.Text == Btn_32.Text)
+            {
+                letra = Btn_12.Text; ponto();
+            }
+            if (Btn_13.Text == Btn_23.Text && Btn_23.Text == Btn_33.Text )
+            {
+                letra = Btn_13.Text; ponto();
+            }
+            if (Btn_11.Text == Btn_22.Text && Btn_22.Text == Btn_33.Text )
+            {
+                letra = Btn_11.Text; ponto();
+            }
+            if (Btn_13.Text == Btn_22.Text && Btn_22.Text == Btn_31.Text )
+            {
+                letra = Btn_13.Text; ponto();
+            }
+            if (Btn_11.Enabled == false && Btn_12.Enabled == false && Btn_13.Enabled == false && Btn_21.Enabled == false && Btn_22.Enabled == false && Btn_23.Enabled == false && Btn_31.Enabled == false && Btn_32.Enabled == false && Btn_33.Enabled == false)
+            {
+                MessageBox.Show("O Jogo EMPATOU");
+                inicio();
+            }
+        }
+        #endregion
+        #region Placar da jogada
+        void ponto()
+        {
+            if (letra == "X")
+            {
+                venceX++;
+                Lbl_Jx.Text = "Jogador X = " + venceX.ToString();
+                MessageBox.Show("Jogador X venceu");
+                inicio();
+            }
+            else if (letra == "O")
+            {
+                venceO++;
+                Lbl_Jo.Text = "Jogador O = " + venceO.ToString();
+                MessageBox.Show("Jogador O venceu");
+                inicio();
+            }
+        }
+        #endregion
+
+    }
+}
